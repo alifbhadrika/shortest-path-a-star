d6d8237da63541a8a35248d523a35bb3bec2d6ca
Alif Bhadrika Parikesit <alifbhadrika@gmail.com>
2021-04-03 23:23:42 +0700

    parsing file

--- src/graph.py
+++ src/graph.py
@@ -19,9 +19,17 @@ class Graph:
     def addVertex(self, _name, _lat, _long):
         newVertex = Vertex(_name, _lat, _long)
         self.vertices.append(newVertex)
-    
+
+    def addEdge(self, v1, v2):
+        idx1 = self.findVertexIdx(v1)
+        idx2 = self.findVertexIdx(v1)
+        self.adj[idx1][idx2] = self.calcDist(v1, v2)
+        
     def syncAdj(self,adjmat):
         self.adj = adjmat
+
+    def findVertexByIdx(self, idx):
+        return self.vertices[idx].name
 
     def findVertexIdx(self, _name):
         for i in range (self.numVertices):
@@ -44,4 +52,26 @@ class Graph:
         a = (math.sin(deltaLat/2))**2 + math.cos(lat1)*math.cos(lat2)*(math.sin(deltaLong/2))**2
         c = 2 * math.asin(math.sqrt(a))
         return 6371 * c * 1000
-    
+
+# file util
+def parseFile(filename):
+    with open('test/'+filename, "r") as file:
+        lines = file.readlines()
+        # get number of vertices from 1st line
+        numVertices = int(lines[0].strip())
+        G = Graph(numVertices)
+
+        # construct every vertex from line 2
+        for i in range(1, numVertices + 1):
+            vertexArgs = lines[i].strip().split(" ")
+            G.addVertex(vertexArgs[0], float(vertexArgs[1]), float(vertexArgs[2]))
+
+        # construct every edges from adj matrix
+        for i in range(numVertices + 1, len(lines)):
+            elmt = lines[i].strip().split(" ")
+            rowIdx = i - (numVertices + 1)
+            for j in range(numVertices):
+                if elmt[j] == '1':
+                    G.addEdge(G.findVertexByIdx(rowIdx), G.findVertexByIdx(j))
+
+    return G
